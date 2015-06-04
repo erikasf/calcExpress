ea2dc6b8dcfe3d0724fda71fba6be795f8bc67f3
erika <erika@erikasf.com>
2015-06-04 03:11:24 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+
+var express = require ("express");
+
+var app = express();
+
+
+app.set("view engine", "ejs");
+
+app.get("/add/:num1/:num2", function(request, response){
+	var result = request.params.num1 + request.params.num2;
+	response.render("math", {number:result});
+});
+
+app.get("/subract/:num1/:num2", function(request, response){
+	var result = request.params.num1 - request.params.num2;
+	response.render("math", {number:result});
+});
+
+app.get("/multiply/:num1/:num2", function(request, response){
+	var result = request.params.num1 * request.params.num2;
+	response.render("math", {number:result});
+});
+
+app.get("/divide/:num1/:num2", function(request, response){
+	var result = request.params.num1 / request.params.num2;
+	response.render("math", {number:result});
+});
+
+
+
+app.listen(3000, function(){
+	console.log("Server starting on port 3000");
+})
