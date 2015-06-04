9c06cedc9480e4d2610837c07b92ba4a89c72fd0
erika <erika@erikasf.com>
2015-06-04 10:10:12 -0700

    Update app.js
    updating the routing, after having forgotten to include the path for response.render.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 
 
 app.set("view engine", "ejs");
-response.render("index.html")
+response.render("calc.ejs")
 
 app.get("/add/:num1/:num2", function(request, response){
 	var result = request.params.num1 + request.params.num2;
